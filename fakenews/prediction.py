10f57576987fa4f366f0991c31e41d61ea72b5e4
sohil <sohilayubluhar@gmail.com>
2019-07-19 00:08:42 +0530

    add'

--- fakenews/prediction.py
+++ fakenews/prediction.py
@@ -5,7 +5,7 @@ import pickle
 #function to run for prediction
 def detecting_fake_news(var):    
 #retrieving the best model for prediction call
-	load_model = pickle.load(open('S:/Fake News Detection/Dataset/final_model.sav','rb'))
+	load_model = pickle.load(open('final_model.sav','rb'))
 	prediction = load_model.predict([var])
 	prob = load_model.predict_proba([var])
 	print("The given statement is ",prediction[0])
