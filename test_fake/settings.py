22d57667c6dfd7de57abf389b74f461940726782
sohil <sohilayubluhar@gmail.com>
2019-07-19 10:24:54 +0530

    test

--- test_fake/settings.py
+++ test_fake/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/static/"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CONSUMER_KEY = "eOlN2NmsgzXdMvjGay8QAXdhv"
